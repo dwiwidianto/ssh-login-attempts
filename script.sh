bdd5e950aea63afa4ff99f213ff3d7c23d9c4f8f
widi443 <dwiwidianto00@gmail.com>
2022-01-31 13:30:43 +0700

    get matrix ssh login attempts & send to telegram

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-tail /var/log/auth.log | grep 'user'
+ssh_attempts=$(journalctl _SYSTEMD_UNIT=ssh.service | egrep -c "Failed|Failure">
+get_ip=$(wget -qO- https://ifconfig.me ; echo $var)
+
+massage="$ssh_attempts ssh login attempts were at $get_ip"
+
+./send-telegram.sh "$massage"
